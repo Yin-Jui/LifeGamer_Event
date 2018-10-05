536bccd41362659b25e088af729ce0ea5519741c
Kevin Cyu <kevinbird61@gmail.com>
2018-10-05 11:03:33 +0800

    Add runtime control prototype (#4)
    * Make runtime server run in background
    * Add fetch operation
    * Add runtime-insert function

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	// "log"
+	"net/http"
+	//"io/ioutil"
 	"flag"
 	"strings"
+	"strconv"
+	"encoding/json"
+	"../internal/data"
 	"../internal/parser"
 	"../internal/logger"
 	"../internal/history"
+	"../internal/runtime"
 	"../internal/event/engine"
 )
 
@@ -15,6 +22,8 @@ func main(){
 	itype := flag.String("itype", "json", "Specify type of input file.")
 	ifile := flag.String("ifile", "test.json", "Specify filename of input file.")
 	debug := flag.Bool("debug", false, "Debug flag, default is false.")
+	runtime := flag.Bool("runtime", false, "Runtime server enable/disable, default is disable.")
+	port := flag.Int("port", 9000, "Port number for runtime server.")
 	doom := flag.Float64("doom", 0, "Doom of this simulation.")
 
 	// argparse parsing 
@@ -46,20 +55,97 @@ func main(){
 
 	// get something useful from source file
 	// create engine & initialize it
-	engine := engine.Engine{}
-	engine.Init(0, *doom, p.Obj)
+	e := engine.Engine{}
+	e.Init(0, *doom, p.Obj)
 
 	// start simulation process
-	engine.Start()
-
-	// fmt.Println(engine.History)
+	e.Start()
 
 	// assign history event list into engine
 	history := history.History{}
-	history.Init(0, *doom, engine.History)
+	history.Init(0, *doom, e.History)
 
+	// Print out total event summation 
 	fmt.Println(history.Map)
 
-	// fmt.Println("Poisson: ",engine.Debug_poisson())
-	// fmt.Println("Exponential: ",engine.Debug_expon())
+	// ================================================= 
+	/* 
+	 * Runtime control 
+	 * - if not specify runtime flag, then event engine will not support runtime controll
+	 * - only support output "history"
+	*/
+	// =================================================
+	if *runtime {
+		// create monitor, aim for peaker program/runtime control 
+		app := monitor.CreateMonitor()
+
+		// Handler
+		/*
+			/health: 	[GET] health check
+			/fetch:		[GET] get the history
+			/insert:	[POST] insert new event into history
+		*/
+		app.Handle(`^/health$`, func(ctx *monitor.Context){
+			// fmt.Println(ctx.Request.Method)
+			ctx.Text(http.StatusOK, "text/plain", "OK")
+		})
+		app.Handle(`^/fetch$`, func(ctx *monitor.Context){
+			// return all 
+			jsonstr,_ := json.Marshal(history)
+			ctx.Text(http.StatusOK, "text/plain", fmt.Sprintf("%s", string(jsonstr)))
+		})
+		app.Handle(`^/insert`, func(ctx *monitor.Context){
+			ctx.Request.ParseForm()
+			// insert new event 
+			if ctx.Request.Method == http.MethodPost {
+				// TODO
+				// schedule new event into history
+				var NewEvent data.Object
+				for key,value := range ctx.Request.Form {
+					// fmt.Printf("%s = %s\n", key, value)
+					switch key {
+						case "event_name":
+							NewEvent.Name = value[0]
+						case "event_model":
+							NewEvent.Model = value[0]
+						case "lambda":
+							NewEvent.Lambda,_ = strconv.Atoi(value[0])
+						case "x": 
+							NewEvent.X,_ = strconv.Atoi(value[0])
+						default:
+							ctx.Text(404, "text/plain", "Error input form.")
+					}
+				}
+
+				fmt.Println(NewEvent)
+				var event_arr []data.Object
+				event_arr = append(event_arr, NewEvent)
+				// And then regenerate Event 
+				newEngine := engine.Engine{}
+				newEngine.Init(0, *doom, event_arr)
+				newEngine.Start()
+				// Push new event into history
+				history.Add_event_list(newEngine.History)
+
+				// Print out total event summation 
+				fmt.Println(history.Map)
+
+			} else {
+				ctx.Text(http.StatusMethodNotAllowed, "text/plain", "Please using POST method")
+			}
+		})
+
+		fmt.Println("Runtime server starting ...")
+		// Running server in the background
+		go func() {
+			http.ListenAndServe(fmt.Sprintf(":%d", *port), app)
+		}()
+	}
+
+	/*
+		Dealing with other things
+	*/
+
+	// need an endless loop to maintain background server 
+	for {}
 }
